c29f846139b65407c96bac7236ac76119cbd36b4
Kevin <kfachas@gmail.com>
2021-08-13 00:30:46 +0200

    add GooglePlacesAutocomplete

--- containers/AroundMeScreen.js
+++ containers/AroundMeScreen.js
@@ -1,6 +1,6 @@
 // react:js/native
 import React, { useState, useEffect } from "react";
-import { Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { Text, SafeAreaView, View } from "react-native";
 import Distance from "../components/Distance";
 // Icons
 import { Ionicons } from "@expo/vector-icons";
@@ -10,10 +10,15 @@ import { Dimensions } from "react-native";
 const width = Dimensions.get("window").width;
 const height = Dimensions.get("window").height;
 //  Location
-import * as Location from "expo-location";
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-// import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { getDistance, convertDistance } from "geolib";
+import Location, { installWebGeolocationPolyfill } from "expo-location";
+import MapView, {
+  Callout,
+  Circle,
+  Marker,
+  PROVIDER_GOOGLE,
+} from "react-native-maps";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+// navigator.geolocation = require("@react-native-community/geolocation");
 // request
 import axios from "axios";
 
@@ -29,7 +34,7 @@ export default function AroundMeScreen({ navigation }) {
       try {
         const { status } = await Location.requestForegroundPermissionsAsync();
         if (status === "granted") {
-          const location = await Location.getCurrentPositionAsync();
+          // const location = await Location.getCurrentPositionAsync();
           setLongitude(2.28322);
           setLatitude(48.863918);
           setOkay(true);
@@ -73,14 +78,41 @@ export default function AroundMeScreen({ navigation }) {
     />
   ) : (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={{ height: 100 }}>
+        <GooglePlacesAutocomplete
+          styles={{
+            container: { flex: 1 },
+          }}
+          placeholder="Search locations"
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            // details exist if fetchDetails = true
+            console.log("DETAILS =>> ", Object.key(details));
+          }}
+          query={{
+            key: "AIzaSyALKaefFkdtwubPa1iOhQbhWZKGTlsnIYI",
+            language: "fr",
+            components: "country:fr",
+          }}
+          enablePoweredByContainer={true}
+          autoFocus={false}
+          returnKeyType={"default"}
+          onNotFound={() => {
+            console.log("t nul");
+          }}
+          minLength={3}
+          // currentLocation={true}
+          // currentLocationLabel="Current location"
+        />
+      </View>
       <MapView
-        style={{ width, height }}
+        style={{ width, height: 600 }}
         provider={PROVIDER_GOOGLE}
         initialRegion={
           okay
             ? {
-                latitude: 48.863918, // User
-                longitude: 2.28322,
+                latitude: latitude, // User
+                longitude: longitude,
                 latitudeDelta: 0.1,
                 longitudeDelta: 0.1,
               }
@@ -95,12 +127,41 @@ export default function AroundMeScreen({ navigation }) {
         {okay && (
           <Marker
             coordinate={{
-              latitude: 48.863918, // User
-              longitude: 2.28322,
+              latitude, // User
+              longitude,
             }}
-            title="Your position"
-          ></Marker>
+            call
+            draggable={true}
+            pinColor="black"
+            onDragStart={(e) => {
+              console.log("Drag start", e.nativeEvent.coordinate);
+            }}
+            onDragEnd={(e) => {
+              console.log("Drag end", e.nativeEvent.coordinate);
+            }}
+            style={{ alignItems: "center" }}
+          >
+            <View
+              style={{ backgroundColor: "white", borderRadius: 10, padding: 5 }}
+            >
+              <Text
+                style={{
+                  padding: 2,
+                }}
+              >
+                I'm here
+              </Text>
+            </View>
+            <Ionicons name="location" size={35} color="#F9575C" />
+          </Marker>
         )}
+        <View>
+          <Circle
+            center={{ longitude, latitude }}
+            radius={5000}
+            fillColor="rgba(64,224,208, 0.5)"
+          />
+        </View>
         {coords.map((elem, index) => {
           return (
             <Marker
@@ -118,6 +179,8 @@ export default function AroundMeScreen({ navigation }) {
               }}
             >
               <Text
+                numberOfLines={1}
+                ellipsizeMode="tail"
                 style={{
                   textAlign: "center",
                   backgroundColor: "white",
@@ -134,7 +197,7 @@ export default function AroundMeScreen({ navigation }) {
                   longitude={longitude}
                 />
               )}
-              <Ionicons name="location" size={24} color="red" />
+              <Ionicons name="location" size={24} color="black" />
             </Marker>
           );
         })}
